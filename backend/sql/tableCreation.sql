172d0272a7cb976c19cd23c122a03ae612f1d1ec
Kevin <kevin3ngeleka@gmail.com>
2024-11-17 13:46:52 -0500

    wht i said b4

--- backend/sql/tableCreation.sql
+++ backend/sql/tableCreation.sql
@@ -3,3 +3,5 @@ CREATE TABLE students (studentID int, password varchar(15), gender varchar(8), f
 CREATE TABLE players (playerID int, studentID int, teamID int, PRIMARY KEY (playerID), FOREIGN KEY (studentID) references students(studentID);
 CREATE TABLE teams (name varchar(30), teamID int, leagueID int, numPlayers int DEFAULT 1, captainSID int, wins int DEFAULT 0, losses int DEFAULT 0, PRIMARY KEY (teamID), FOREIGN KEY (leagueID) references leagues(leagueID),
 FOREIGN KEY (captainSID) references students(studentID); 
+CREATE TABLE joinRequests (requestID int, studentID int, teamID int, requestTime TIMESTAMP DEFAULT CURRENT_TIMESTAMP, 
+PRIMARY KEY(requestID), FOREIGN KEY(studentID) references students(studentID), FOREIGN KEY (teamID) references teams(teamID));
---
+++ client/src/components/LeagueTeams.jsx
@@ -0,0 +1,52 @@
+
+import { useState, useEffect, useRef } from "react"
+import {TeamTableEntry} from './Home'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import {faVolleyball, faBasketball, faFutbol, faPersonRunning} from '@fortawesome/free-solid-svg-icons'
+import { useNavigate } from "react-router-dom"
+import { useStudentStore, useCurrLeagueStore, useCurrTeamStore } from "../Stores"
+import {PropTypes} from 'prop-types'
+import { useShallow } from 'zustand/react/shallow'
+
+export const LeagueTeams = ({leagueObj, teams}) => {
+    const sport = leagueObj.sport;
+    const numTeams = leagueObj.numTeams;
+    const maxTeams = leagueObj.maxTeams;
+    const maxPlayers = leagueObj.maxPlayers;
+    const gender = leagueObj.genders;
+    const level = leagueObj.skillLevel;
+    const leagueID = leagueObj.leagueID
+    let indices = [...Array(teams.length).keys()]
+    const effectRan = useRef(false);
+    
+    useEffect(()=>{
+        if(!effectRan.current){
+            // const fetchTeams = async() => {
+            //     {
+            //         const url = `http://localhost:8800/teams/league/${leagueID}`
+            //         const result = await fetch(url);
+            //         result.json().then(json => {
+            //             setTeams(json)
+            //         })
+            //     }
+            // }
+            // fetchTeams();
+            
+            return()=>{
+                effectRan.current = true;
+            }
+        }
+    })
+    return(
+        <>
+            {effectRan.current && indices.map((e) => {
+                return <TeamTableEntry key={e} view="League" teamObj={teams.sort(({wins:a}, {wins:b}) => b-a).at(e)}></TeamTableEntry>
+            })}
+        </>
+    )
+}
+
+LeagueTeams.propTypes={
+    leagueObj: PropTypes.object,
+    teams: PropTypes.array
+}
