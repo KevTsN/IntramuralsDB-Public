b6dec3b26c9634315be00c4c3dd556e1f6e7b300
Kevin <kevin3ngeleka@gmail.com>
2024-11-19 04:57:51 -0500

    yo

--- backend/src/routes copy.js
+++ backend/src/routes copy.js
@@ -513,15 +513,6 @@ export async function addPlayerByReq(req,res){
 }
 
 
-export const fetchAll = async (db, sql, params) => {
-  return new Promise((resolve, reject) => {
-    db.all(sql, params, (err, rows) => {
-      if (err) reject(err);
-      resolve(rows);
-    });
-  });
-};
-
 
 /*
 db.exec(q, function(err){
