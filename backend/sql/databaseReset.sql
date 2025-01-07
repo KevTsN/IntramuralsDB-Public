724a95bb7f33676abef6e78d9204b6e412b57ae5
Kevin <kevin3ngeleka@gmail.com>
2025-01-07 13:32:41 -0500

    init commit for public

--- backend/sql/databaseReset.sql
+++ backend/sql/databaseReset.sql
@@ -1,5 +1,7 @@
 delete from players;
 delete from teams;
+delete from students;
 update leagues set numTeams = 0;
 delete from join_requests;
 delete from games;
+delete from invites;
--- readme.txt
+++
@@ -1,2 +0,0 @@
-Database: /backend/sql/database.db
-YouTube Link: https://youtu.be/LRcoYWhJY9A?si=ezI2SXzPkF9HaIGV
